579d538f0d9723ffe59a937125d5e9b726c4c670
Richard Sugg <richard.sugg@sas.com>
2015-07-08 16:41:43 -0400

    added env files

---
+++ dev.env.sh
@@ -0,0 +1,5 @@
+export APP_DIR=/src
+export MAX_REPS=10
+export SLEEP_TIME=1
+export SECRET=boo
+export GREET_NAME="race fans!"
---
+++ prod.env.sh
@@ -0,0 +1,5 @@
+export APP_DIR=/src
+export MAX_REPS=5
+export SLEEP_TIME=2
+export SECRET=boohoo
+export GREET_NAME="docker fans!"
